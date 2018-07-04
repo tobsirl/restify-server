70b9b92e734c9823f19cae14157eb9f2feb1107d
Paul Tobin <tobster111@gmail.com>
2018-07-04 12:06:00 +0100

    Added Model, DB connection

---
+++ models/Items.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  quanity: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model('item', ItemSchema);
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-var restify = require('restify');
+import restify from 'restify';
+import mongoose from 'mongoose';
 
 function respond(req, res, next) {
   res.send('hello ' + req.params.name);
   next();
 }
 
+// Connect to database
+mongoose
+  .connect('mongodb://localhost:27017/restify-item')
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
 var server = restify.createServer();
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
-server.listen(8080, function() {
+server.listen(3000, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
